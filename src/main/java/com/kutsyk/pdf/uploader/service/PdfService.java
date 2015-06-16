f8a625e343d5234fa6f22afea19fee9429e2c48d
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2015-06-16 13:49:04 +0300

    pdf service interface

--- src/main/java/com/kutsyk/pdf/uploader/service/PdfService.java
+++ src/main/java/com/kutsyk/pdf/uploader/service/PdfService.java
@@ -5,6 +5,7 @@ import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -15,8 +16,7 @@ public interface PdfService {
     void uploadFile(MultipartHttpServletRequest request);
     List<CloudBlob> getAllFiles();
 
-    void setPasswordToPdfFile();
-    void loadFileToStorage(String result);
-    void changeFileMetaData(String outputFile) throws IOException;
-    void sendFileToStorage(String fileName);
+    void loadFileToStorage(File result);
+    void changeFileMetaData(File source, File result) throws IOException;
+    void setPasswordToPdfFile(File source, File result);
 }
