14fad7110048456623f900102d3db1ebe956a00a
Ivano Polomani <ivano.polomani@gmail.com>
2015-07-17 11:04:47 +0300

    deleted extra db versions

--- src/main/resources/db/migration/V5__new_permissions.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `nextbook`.`role_permissions` (`role_id`, `permission_id`) VALUES ('2', '1');
-INSERT INTO `nextbook`.`role_permissions` (`role_id`, `permission_id`) VALUES ('3', '1');
-INSERT INTO `nextbook`.`role_permissions` (`role_id`, `permission_id`) VALUES ('4', '1');
-INSERT INTO `nextbook`.`role_permissions` (`role_id`, `permission_id`) VALUES ('5', '1');
-INSERT INTO `nextbook`.`role_permissions` (`role_id`, `permission_id`) VALUES ('5', '4');
