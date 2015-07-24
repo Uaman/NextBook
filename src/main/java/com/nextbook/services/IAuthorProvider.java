61068d380d97b2421230d1249b4319555f3bb8b1
Stacy <stace1995D@gmail.com>
2015-07-24 14:28:44 +0300

    New interface added
    IAuthorProvider to be implemented

---
+++ src/main/java/com/nextbook/services/IAuthorProvider.java
@@ -0,0 +1,28 @@
+package com.nextbook.services;
+
+import com.nextbook.domain.pojo.Author;
+import java.util.*;
+
+/**
+ * Created by Stacy on 7/24/15.
+ */
+public interface IAuthorProvider {
+
+    boolean addAuthor(Author author);
+
+    Author updateAuthor(Author author);
+
+    /**
+     * All books connected with specific author need to be removed too, when this method is called?
+     * @param authorId
+     * @return
+     */
+    boolean deleteAuthor(int authorId);
+
+    Author getById(int authorId);
+
+    List<Author> getAll();
+
+    List<Author> getFromMax(int from, int max);
+
+}
