0e9cbbd13f2970bb2bbd4eaeaf6f5158d5735920
Stacy <stace1995D@gmail.com>
2015-07-30 12:36:21 +0300

    Books category added to db

---
+++ src/main/resources/db/migration/V5_add_book_categories.sql
@@ -0,0 +1,19 @@
+-- -----------------------------------------------------
+-- Data for `nextbook`.`CATEGORY` using 4NB
+-- -----------------------------------------------------
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Мистецтво та культура','Искусство и культура','Arts and entertainment');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Біографії і мемуари','Биографии и мемуары','Biographies & memoirs');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Книги іноземними мовами','Книги на иностранных языках','Books in foreign languages');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Бізнес','Бизнес','Business & personal finance');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Діти і підлітки','Дети и подростки','Children & teens');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Комп''ютери та інтернет','Компьютеры и интернет','Computers & internet');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Поварені книги, їжа і вино','Поваренные книги, еда и вино','Cookbooks, food & wine');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Освіта','Образование','education');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Художня література','Художественная литература','Fiction & literature');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Здоров''я, розум і тіло','Здоровье, ум и тело','Health, mind & body');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Історія','История','history');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Спосіб життя і домашнє господарство','Образ жизни и домашнее хозяйство','Lifestyle & home');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Професійний і технічний','Профессиональный и технический','Professional & technical');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Довідники','Справочники','Reference');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Релігія і духовність','Религия и духовность','Religion & spirituality');
+INSERT INTO `nextbook`.`CATEGORY` (`name_ua`,`name_ru`,`name_en`) VALUES ('Спорт та  відпочинок','Спорт и отдых','Sports & outdoors');
