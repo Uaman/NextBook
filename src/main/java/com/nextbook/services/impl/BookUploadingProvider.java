24aa656e81d7be3f0da46569808231354f5c0d27
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2015-09-27 16:20:54 +0300

    fix import

--- src/main/java/com/nextbook/services/impl/BookUploadingProvider.java
+++ src/main/java/com/nextbook/services/impl/BookUploadingProvider.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
 import java.util.*;
 
 
